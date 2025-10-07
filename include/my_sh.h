0fc6d870d815dbd931ba60200af5d24503f98987
Saltyy <clement.bouret@epitech.eu>
2025-10-07 17:34:12 +0200

    [Feat/.h] .h proto

---
+++ include/my_sh.h
@@ -0,0 +1,29 @@
+#ifndef MY_SH_H
+#define MY_SH_H
+
+// Standard library includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+// Prototypes des builtins
+int my_cd(char **args, char ***env);
+int my_setenv(char **args, char ***env);
+int my_unsetenv(char **args, char ***env);
+int my_env(char **args, char **env);
+int my_exit(char **args);
+int sh_loop();
+
+
+// Structure pour les builtins
+typedef struct {
+    char *name;
+    int (*func)(char **, char ***);
+} builtin_t;
+
+#endif // MY_SH_H
