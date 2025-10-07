5ad6ff263c3893140a0a6a6dc2ef312cee125041
Saltyy <clement.bouret@epitech.eu>
2025-10-08 00:12:35 +0200

    [Feat/main] updated main

--- src/main.c
+++ src/main.c
@@ -2,9 +2,19 @@
 
 int main(int argc, char **argv, char **envp)
 {
+    char **env;
+    int status;
+
     (void)argc;
     (void)argv;
-    (void)envp;
-    sh_loop();
-    return (0);
+
+    env = copy_env(envp);
+    if (!env) {
+    write(2, "Error: malloc failed\n", 21);
+    return 84;
+    }
+    
+    status = sh_loop(envp);
+    free_env(env);
+    return status;
 }
