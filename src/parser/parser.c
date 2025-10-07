afe38a3674569f2510eaec14cd3e0f7282231cc7
Saltyy <clement.bouret@epitech.eu>
2025-10-08 00:09:30 +0200

    [Feat/parser] basic line parser for my shell

---
+++ src/parser/parser.c
@@ -0,0 +1,33 @@
+#include "my_sh.h"
+
+char **parse_line(char *line)
+{
+    char **args = NULL;
+    char *token;
+    int count;
+
+    char *line_cp = malloc(strlen(line) + 1);
+    strcpy(line_cp, line);
+    token = strtok(line_cp, "\t\n");
+    
+    while(token)
+    {
+        count++;
+        token = strtok(NULL, "\t\n");
+    }
+    free(line_cp);
+
+    args = malloc(sizeof(char *) * (count + 1));
+    count = 0;
+    token = strtok(line, "\t\n");
+
+    while(token)
+    {
+        args[count] = malloc(strlen(token) + 1);
+        strcpy(args[count], token);
+        count++;
+        token = strtok(NULL, "\t\n");
+    }
+    args[count] = NULL;
+    return args;
+}
