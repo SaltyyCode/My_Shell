dfed8dce4aff1430cf4ef38e24d3e1c5a9c350c6
Saltyy <clement.bouret@epitech.eu>
2025-10-09 16:01:29 +0200

    [Fix/parser] fixed error in parser missing space in strtok

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -4,29 +4,29 @@ char **parse_line(char *line)
 {
     char **args = NULL;
     char *token;
-    int count;
+    int count = 0;
 
     char *line_cp = malloc(strlen(line) + 1);
     strcpy(line_cp, line);
-    token = strtok(line_cp, "\t\n");
+    token = strtok(line_cp, " \t\n");
     
     while(token)
     {
         count++;
-        token = strtok(NULL, "\t\n");
+        token = strtok(NULL, " \t\n");
     }
     free(line_cp);
 
     args = malloc(sizeof(char *) * (count + 1));
     count = 0;
-    token = strtok(line, "\t\n");
+    token = strtok(line, " \t\n");
 
     while(token)
     {
         args[count] = malloc(strlen(token) + 1);
         strcpy(args[count], token);
         count++;
-        token = strtok(NULL, "\t\n");
+        token = strtok(NULL, " \t\n");
     }
     args[count] = NULL;
     return args;
