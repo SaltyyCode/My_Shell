d72517241ec269d6e9173375aaf45186abf3f058
Saltyy <clement.bouret@epitech.eu>
2025-10-09 15:54:30 +0200

    [Feat/sh] updated .h

--- include/my_sh.h
+++ include/my_sh.h
@@ -1,7 +1,6 @@
 #ifndef MY_SH_H
 #define MY_SH_H
 
-// Standard library includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,28 +8,33 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/utsname.h>
 #include <signal.h>
+#include <time.h>
+#include <pwd.h>
+#include "builtins/builtins.h"
 
-// Prototypes des builtins
+#define BOLD "\033[1m"
+#define CYAN "\033[0;36m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
+
+
 int my_cd(char **args, char ***env);
-int my_setenv(char **args, char ***env);
-int my_unsetenv(char **args, char ***env);
-int my_env(char **args, char **env);
-char **copy_env(char **env);
-void free_env(char **env);
-int my_exit(char **args);
+int my_exit(char **args, char ***env);
+void print_error(char const *str);
+char *get_current_dir(void);
 
-int sh_loop(char **env);
+int sh_loop(char ***env);
+void print_shell_banner_compact(char **env);
+void  print_shell_info(void);
 
-int is_builtin(char *command);
-int execute_builtin(char **args, char ***env);
+void free_args(char **args);
 
 char **parse_line(char *line);
-
-// Structure pour les builtins
-typedef struct {
-    char *name;
-    int (*func)(char **, char ***);
-} builtin_t;
 
 #endif // MY_SH_H
