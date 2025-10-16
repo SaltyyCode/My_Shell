7ad7823a249f0047aebb8ee4a89bbba7c38b39ef
Saltyy <clement.bouret@epitech.eu>
2025-10-16 10:26:20 +0200

    [Feat/env.h] new functions to help cd with env var

--- include/env/env.h
+++ include/env/env.h
@@ -16,5 +16,9 @@ int my_env(char **args, char **env);
 char **copy_env(char **env);
 void free_env(char **env);
 char *get_env_value(const char *name, char **env);
+char *create_env_entry(const char *key, const char *value);
+int update_existing_var(char **env, const char *key, char *new_entry);
+void update_env_var(const char *name, const char *value, char ***env);
+
 
 #endif // ENV_H
